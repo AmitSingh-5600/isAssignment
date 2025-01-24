c218173ed384c45dd71bd07169b51e19f0cc829a
Amit Singh Kushwaha <136868174+AmitSingh-5600@users.noreply.github.com>
2025-01-24 22:15:07 +0530

    1a

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,19 +5,17 @@ const Cart = () => {
     <>
       <div className="flex-col">
         <h3 className="text-2xl text-center underline p-4">Items in cart</h3>
-        <div className="mx-auto grid gap-10 w-2/6 ">
+        <div className="mx-auto w-full grid gap-10 sm:w-120">
           {productList.products.map((product) => (
             <div
               key={product.id}
-              className="flex  bg-slate-100 p-4 justify-between"
+              className="flex  bg-slate-100 p-4 justify-center"
             >
-              <img
-                className="h-10 sm:h-50 "
-                src={product.imageUrl}
-                alt="product"
-              />
+              <div className="w-1/2 flex justify-end mr-4">
+                <img className="h-30 " src={product.imageUrl} alt="product" />
+              </div>
 
-              <div className="w-3/6">
+              <div className="w-1/2 ml-4">
                 <h4>{product.name}</h4>
                 <p>{product.detail}</p>
                 <button className="bg-blue-200 rounded-xl p-3 mt-2">
