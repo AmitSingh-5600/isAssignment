ec9b538b3ce90a903a9d95541adf3d491d22dadf
Amit Singh Kushwaha <136868174+AmitSingh-5600@users.noreply.github.com>
2025-01-24 21:39:16 +0530

    fix navlink issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <div className=" bg-blue-300 h-18 flex gap-10 py-3 px-12 items-center ">
-      <NavLink to="/">
+      <NavLink to="/isAssignment/">
         <FaHome className="size-8 text-white" />
       </NavLink>
       <div className="flex flex-1 justify-end gap-10">
-        <NavLink to="/add">
+        <NavLink to="/isAssignment/add">
           <LuLayoutDashboard className="size-8 text-white" />
         </NavLink>
-        <NavLink to="/cart" className="flex">
+        <NavLink to="/isAssignment/cart" className="flex">
           <FaShoppingCart className="size-8 text-white" />
           <p className="bg-white w-5 h-5  text-center  rounded-full relative -left-2.5 -top-2">
             7
